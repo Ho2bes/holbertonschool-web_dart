1c608ca53fd91b5845284e2769ab4b0dd0e2847e
Ho2bes <8696@holbertonstudents.com>
2025-07-18 11:44:24 +0200

    feat: add isPalindrome function to check for palindromic strings

---
+++ 0x01-dart-function/9-palindrome.dart
@@ -0,0 +1,10 @@
+bool isPalindrome(String s)
+{
+  if (s.length < 3) return false;
+
+  String cleaned = s.replaceAll(' ', '').toLowerCase();
+
+  String reversed = cleaned.split('').reversed.join('');
+
+  return cleaned == reversed
+}
