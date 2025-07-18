14b129beb0662244b5e3d0555185c19f466ea0f9
Ho2bes <8696@holbertonstudents.com>
2025-07-18 13:40:53 +0200

    feat: add Password class with validation and string representation

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,26 @@
+class Password {
+   String _password = "";
+
+  Password({required String password}) {
+    _password = password;
+  }
+
+  String get password => _password;
+
+  set password(String value) {
+    _password = value;
+  }
+
+  bool isValid() {
+    final hasUppercase = _password.contains(RegExp(r'[A-Z]'));
+    final hasLowercase = _password.contains(RegExp(r'[a-z]'));
+    final lengthOk = _password.length >= 8 && _password.length <= 16;
+
+    return hasUppercase && hasLowercase && lengthOk;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
